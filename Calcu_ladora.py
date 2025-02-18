8fe2c22f050a8cd6f5253a38c7bdc9676384486b
Fer guerrero <luisfernandoguerrero62@gmail.com>
2025-02-18 17:53:56 -0600

    Agrege mi parte al codigo (la suma)

--- Calcu_ladora.py
+++ Calcu_ladora.py
@@ -1,6 +1,12 @@
 import math
 
+def suma(a, b):
+    return a + b
+
 def division(a, b):
     if b == 0:
         return "Error: División por cero"
     return a / b
+
+if __name__ == "__main__":
+    print("Suma de 5 y 3:", suma(5, 3))
