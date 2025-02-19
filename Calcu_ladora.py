47a729db68e9eda1a5793b1422e1eabf6a4dbe6f
Carlos <urielarenas654@gmail.com>
2025-02-18 18:42:05 -0600

    Se agrego la Multiplicacion

--- Calcu_ladora.py
+++ Calcu_ladora.py
@@ -11,5 +11,9 @@ def division(a, b):
         return "Error: División por cero"
     return a / b
 
+def multiplicacion(a,b):
+    return a * b
+
 if __name__ == "__main__":
     print("Suma de 5 y 3:", suma(5, 3))
+    print("Multiplicación de 3 y 6:", multiplicacion(3, 6))
